97f768364366a464268661b8834ab5a78751e238
Ishan Veer <ishanpveer@gmail.com>
2025-07-15 23:28:57 +0530

    setup tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      hello world
+    <div className="text-light-200 bg-dark-300 py-20">
+      <p className="text-light-200">sample text 1</p>
+      <p className="text-light-300">Sample text 2</p>
+      <p className="text-light-100">Sample text 3</p>
+      <p className="text-light-400">Sample text 4</p>
     </div>
   );
 }
